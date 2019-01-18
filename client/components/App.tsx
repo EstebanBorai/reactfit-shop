c88fdf8a08a950549c9c30f964d0413e0818cc11
Esteban Borai <estebanborai@outlook.com>
2019-01-17 23:42:04 -0300

    Add sockets.io basic setup

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import Header from "./Header";
+import { connect as io } from 'socket.io-client';
 
 class App extends React.Component {
+  componentDidMount() {
+    io('http://localhost:3000');
+  }
   render() {
     return (
       <div>
