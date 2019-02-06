7d24712c9bd442a6e6150fb0e42dea5b175a4fc9
Esteban Borai <estebanborai@outlook.com>
2019-02-05 21:50:46 -0300

    Add mongo session

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const io = require('socket.io');
 const startSockets = require('./startSockets');
 
@@ -11,21 +14,29 @@ const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
 const HTTPServer = http.Server(app);
 const sockets = io(HTTPServer);
+const mongoConnection = 'mongodb://localhost/lobby_app_data';
+
+if(isProduction) {
+  mongoose.connect(mongoConnection);
+} else {
+  mongoose.connect(mongoConnection, { useNewUrlParser: true });
+  mongoose.set('debug', true);
+}
 
 app.use(cors());
+app.use(session({
+  secret: 'session-secret',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 if (!isProduction) {
   app.use(errorhandler());
-}
-
-if(isProduction) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/lobby_app_data');
-  mongoose.set('debug', true);
 }
 
 require('./models/User');
